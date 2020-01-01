1ec78a98b9b0c3869befbce6c88b6213b684fe8f
Antonio <toninho0007@yahoo.com.br>
2019-12-31 23:10:36 -0300

    Adding version to component

--- component.go
+++ component.go
@@ -1,8 +1,11 @@
 package ecs_engine
 
+import "sync/atomic"
+
 type BaseComponent struct {
 	EntityId      uint32
 	ComponentType uint16
+	Version       int64
 }
 
 func (b *BaseComponent) SetEntityId(id uint32) {
@@ -17,8 +20,18 @@ func (b *BaseComponent) GetComponentType() uint16 {
 	return b.ComponentType
 }
 
+func (b *BaseComponent) GetVersion() int64 {
+	return b.Version
+}
+
+func (b *BaseComponent) UpdateVersion() {
+	atomic.AddInt64(&b.Version, 1)
+}
+
 type Component interface {
 	SetEntityId(id uint32)
 	GetEntityId() uint32
 	GetComponentType() uint16
+	GetVersion() int64
+	UpdateVersion()
 }
